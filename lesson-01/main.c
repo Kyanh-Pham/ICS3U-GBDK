7d0ef1a07fde1c8a8d0eeb8c456a77ca2664f140
Kyanh Pham <kyanh.pham@mths.ca>
2022-12-22 15:47:28 +0000

    all new files

---
+++ lesson-01/main.c
@@ -0,0 +1,12 @@
+// Copyright (c) 2022 Kyanh Pham
+//
+// Created by: Kyanh Pham
+// Created on: Dec 2022
+// This program is the Space Aliens game for the Gameboy
+
+#include <gb/gb.h>
+#include <stdio.h>
+
+void main(){
+    printf("Hello, World!");
+}
